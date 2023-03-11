efcd302d741e19987ff0b529b62cd1c527fef335
ARmax34 <a34rosenberg@gmail.com>
2023-03-11 00:50:45 -0500

    upload 1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime as dt
 import numpy as np
-import pandas as pd
+
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -11,15 +11,16 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite") #create engine to call to sqlite database
 
 # reflect an existing database into a new model
-Base = automap_base()
+Base = automap_base() #creates internal copy/object of database
+
 # reflect the tables
 Base.prepare(autoload_with=engine)
 
 # Save references to each table
-Measurement = Base.classes.measurement
+Measurement = Base.classes.measurement 
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
@@ -28,7 +29,7 @@ session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__name__)
+app = Flask(__name__) #create class called name
 
 
 #################################################
@@ -39,16 +40,33 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
+        f"Welcome to Hawaii Climate Page<br/> "
         f"Available Routes:<br/>"
+        f"<br/>"  
+        f"The list of precipitation data with dates:<br/>"
         f"/api/v1.0/precipitation<br/>"
+        f"<br/>"
+        f"The list of stations and names:<br/>"
         f"/api/v1.0/stations<br/>"
+        f"<br/>"
+        f"The list of temperatures observations from a year from the last data point:<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/temp<br/>"
+        f"<br/>"
+        f"Min, Max. and Avg. temperatures for given start date: (please use 'yyyy-mm-dd' format):<br/>"
+        f"/api/v1.0/min_max_avg/&lt;start date&gt;<br/>"
+        f"<br/>"
+        f"Min. Max. and Avg. temperatures for given start and end date: (please use 'yyyy-mm-dd'/'yyyy-mm-dd' format for start and end values):<br/>"
+        f"/api/v1.0/min_max_avg/&lt;start date&gt;/&lt;end date&gt;<br/>"
+        f"<br/>"
+        f"i.e. <a href='/api/v1.0/min_max_avg/2012-01-01/2016-12-31' target='_blank'>/api/v1.0/min_max_avg/2012-01-01/2016-12-31</a>"
     )
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    #create session
+    # session = Session(engine)
+
     """Return the precipitation data for the last year"""
     # Calculate the date 1 year ago from last date in database
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
@@ -56,36 +74,37 @@ def precipitation():
     # Query for the date and precipitation for the last year
     precipitation = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prev_year).all()
-
+    
     session.close()
-    # Dict with date as the key and prcp as the value
 
-    # Save the query results as a Pandas DataFrame and set the index to the date column
-    results_df = pd.DataFrame(precipitation, columns=['date', 'prcp'])
+    # Create dictionary with date as the key and prcp as the value
+    precipitation_data = []
+    for date, prcp in precipitation:
+        precipitation_dict = {}
+        precipitation_dict ["date"] = date
+        precipitation_dict ["precipitation"]= prcp
+        precipitation_data.append(precipitation_dict)
 
-
-    # Sort the dataframe by date
-    results_df = results_df.sort_values("date", ascending=False).reset_index(drop=True)
-    print(results_df)
-    return jsonify(results_df.to_dict())
+    return jsonify(precipitation_data)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
+
     """Return a list of stations."""
+    #query for list of stations
     results = session.query(Station.station).all()
 
     session.close()
 
     # Unravel results into a 1D array and convert to a list
     stations = list(np.ravel(results))
-    #return jsonify(stations)
-    return jsonify(stations=stations)
-## WORK NEEDED HERE ##
+    return jsonify(stations)
 
 
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
+ 
     """Return the temperature observations (tobs) for previous year."""
     # Calculate the date 1 year ago from last date in database
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
@@ -96,6 +115,7 @@ def temp_monthly():
         filter(Measurement.date >= prev_year).all()
 
     session.close()
+
     # Unravel results into a 1D array and convert to a list
     temps = list(np.ravel(results))
 
@@ -103,11 +123,12 @@ def temp_monthly():
     return jsonify(temps=temps)
 
 
-@app.route("/api/v1.0/temp/<start>")
-@app.route("/api/v1.0/temp/<start>/<end>")
-def stats(start=None, end=None):
-    """Return TMIN, TAVG, TMAX."""
+@app.route("/api/v1.0/temps/<start>")
+@app.route("/api/v1.0/temps/<start>/<end>")
 
+def stats(start=None, end=None): 
+
+    """Return TMIN, TAVG, TMAX."""
     # Select statement
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
@@ -127,12 +148,12 @@ def stats(start=None, end=None):
         session.close()
 
         temps = list(np.ravel(results))
-        return jsonify(temps=temps)
-        #return ## WORK NEEDED HERE ##
+        return jsonify(temps)
 
     # calculate TMIN, TAVG, TMAX with start and stop
     ## WORK NEEDED HERE ##
-
+    # end = dt.datetime.strptime(end, "%m/%d/%Y")
+    end = dt.datetime.strptime(end, "%m%d%Y")
     results = session.query(*sel).\
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
@@ -141,9 +162,9 @@ def stats(start=None, end=None):
 
     # Unravel results into a 1D array and convert to a list
     temps = list(np.ravel(results))
-    return jsonify(temps=temps)
-    #return ## WORK NEEDED HERE ##
+    return jsonify(temps)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
